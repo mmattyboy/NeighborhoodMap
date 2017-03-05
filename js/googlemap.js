3e269fe238686a0026bc1c6455b8ecc50f58de09
mmattyboy <aqphoen@csu.fullerton.edu>
2017-03-04 16:59:15 -0800

    changed callAjax function andd used forEach to create new marker and call callAjax

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,21 +1,23 @@
-
 var map;
 var markers = [];
+var infowindow;
 var locations = [
-	{name: "rayray", location: {lat: 34.076377, lng: -118.043273}},
-	{name: "riteAid", location: {lat: 34.074506, lng: -118.040498}},
-	{name: "cvs", location: {lat: 34.090727, lng: -118.015660}},
-	{name: "ktown", location: {lat: 34.059889, lng: -118.296802}},
-	{name: "pcc", location: {lat: 34.143933, lng: -118.119168}}
+    { name: "rayray", location: { lat: 34.076377, lng: -118.043273 } },
+    { name: "riteAid", location: { lat: 34.074506, lng: -118.040498 } },
+    { name: "cvs", location: { lat: 34.090727, lng: -118.015660 } },
+    { name: "ktown", location: { lat: 34.059889, lng: -118.296802 } },
+    { name: "pcc", location: { lat: 34.143933, lng: -118.119168 } }
 ];
 
 function initMap() {
-	var visit = {lat: 34.085386, lng: -118.043585};
+    var visit = { lat: 34.085386, lng: -118.043585 };
 
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: visit,
-		zoom: 12
-	});
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: visit,
+        zoom: 12
+    });
+
+    infowindow = new google.maps.InfoWindow({});
 
     ko.applyBindings(new markerViewModel());
 
@@ -23,64 +25,62 @@ function initMap() {
 
 
 // var Marker = function(name, location) {
-// 	var self = this;
-// 	self.name = name;
-// 	self.location = ko.observable(location);
+//  var self = this;
+//  self.name = name;
+//  self.location = ko.observable(location);
 // };
 
 
 
 
 // this function is needed for purpose of organizing and because of closure in the addListener
-// creates an info window and show the window when the marker is clicked
-function addInfoWindow(marker) {
-	$.ajax({
-		url: "http://api.geonames.org/findNearbyWikipedia?" + "lat=" + locations[0].location.lat + "&lng=" + locations[0].location.lng + "&username=aqphoen" ,
-		success: function(data) {
-			console.log(data);
-			var xmlDoc = $.parseXML(data),
-			$xml = $( xmlDoc ),
-			$wikiEntry = $xml.find("wikipediaUrl");
-			var infowindow = new google.maps.InfoWindow({
-				content: $wikiEntry.text()
-			});
+// creates an initial info window showing a default loading text and update the text to the ajax request when received to the wikiUrl link of nearby locations
+function callAjax(location) {
+    infowindow.setContent('Loading...');
+    infowindow.open(map, location.marker);
+    $.ajax({
+        url: "http://api.geonames.org/findNearbyWikipediaJSON?" + "lat=" + location.location.lat + "&lng=" + location.location.lng + "&username=aqphoen",
+        success: function(data) {
+            var url = data.geonames[0].wikipediaUrl;
 
-			marker.addListener("click", function() {
-				infowindow.open(map, marker);
-			});
-		}
-	});
-	
+            infowindow.setContent('<a target="_blank" href="https://' + url + '">Find out more about nearby locations</a>');
+            infowindow.open(map, location.marker);
+
+        },
+        error: function(response) {
+          infowindow.setContent('Something wrong happened...');
+          infowindow.open(map, location.marker);
+        }
+    });
+
 }
 
 function markerViewModel() {
-	var self = this;
-	self.locArray = ko.observableArray([]);
+    var self = this;
+    self.locArray = ko.observableArray([]);
     self.filter = ko.observable('');
 
-    for(var i = 0; i < locations.length; i ++) {
-		var position = locations[i].location;
-		var title = locations[i].name;
-		var marker = new google.maps.Marker({
-			position: position,
-			map: map,
-			title: title,
-			animate: google.maps.Animation.DROP
-		});
+    // use click binding in the list elements and
+    // add this function:
+    // http://knockoutjs.com/documentation/click-binding.html
+    self.handleClick = function(location) {
+      // the marker is accessible as location.marker
+      // trigger the click event of the marker:
+      // http://stackoverflow.com/questions/6794405/trigger-google-maps-marker-click
+    };
 
-        // task: create an event listener for the marker (search for "google marker event click")
-        // and show an infowindow object when the marker is clicked (search for "google maps infowindow")
-
-        // create new marker object in locations array to store marker object created above
-        locations[i].marker = marker;
-        // push locations into observable array to allow data-bind: text in html
-        self.locArray.push(locations[i]);
-//******** self.locArray.addListener("click", function() {
-        // 	infowindow.open()
-        // })
-        markers.push(marker);
-        addInfoWindow(marker);
-	}
+    locations.forEach(function(location) {
+      location.marker = new google.maps.Marker({
+        position: location.location,
+        map: map,
+        title: location.name,
+        animate: google.maps.Animation.DROP,
+      });
+      location.marker.addListener("click", function() {
+          callAjax(location);
+      });
+      self.locArray.push(location);
+    });
 
 
     // this filterLocations variable contain the array of locations that was typed in the filter input
@@ -91,14 +91,14 @@ function markerViewModel() {
         // - check if self.filter() matches the name of the location
         // - if it matches => add the location to the temporary array
         // - finally, return the temporary array from the function (from the computed observable)
-	    for(var i = 0; i < self.locArray.length; i++) {
-			if(self.filter() == locations[i]) {
-				self.locArray.shift();
-				self.locArray.push(locations[i]);
-			}
-	
-    	}
-	});
+        for (var i = 0; i < self.locArray.length; i++) {
+            if (self.filter() == locations[i]) {
+                self.locArray.shift();
+                self.locArray.push(locations[i]);
+            }
+
+        }
+    });
 
 
 }
